077a140ead74c47f890003b2571119654123b910
Mateusz Kretowski <matkretkurs@gmail.com>
2023-02-23 00:58:26 +0100

    Add weather data fetching from API server

--- src/components/PickCity/PickCity.js
+++ src/components/PickCity/PickCity.js
@@ -1,16 +1,19 @@
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
 import styles from './PickCity.module.scss';
-
 import { useState } from 'react';
 
-const PickCity = () => {
+const PickCity = ({ action }) => {
   const [city, setCity] = useState('');
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    action(city);
+    setCity('');
+  };
   return (
-    <form className={styles.pickCityForm}> 
+    <form className={styles.pickCityForm} onSubmit={handleSubmit}>
       <label>
-        <TextInput placeholder="Enter city name...." value={city} onChange={e => setCity(e.target.value)} />
+        <TextInput placeholder='Enter city name....' value={city} onChange={(e) => setCity(e.target.value)} />
       </label>
       <Button>Search</Button>
     </form>
--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,15 +1,38 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import { useCallback } from 'react';
+import { useState } from 'react';
+const WeatherBox = (props) => {
+  const [weatherData, setWeatherData] = useState('');
 
-const WeatherBox = props => {
+  const handleCityChange = useCallback(
+    (city) => {
+      //useCallback don't refresh chidren element PickCity when parent component WeatherBox is refreshed
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d77f935b2221b70aa3dda57f36069a47&units=metric`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          const weather = {
+            city: data.name,
+            temp: data.main.temp,
+            icon: data.weather[0].icon,
+            description: data.weather[0].main,
+          };
+          setWeatherData(weather);
+        });
+    },
+    [weatherData]
+  );
+
   return (
     <section>
-      <PickCity />
-      <WeatherSummary />
+      <PickCity action={handleCityChange} />
+      <WeatherSummary {...weatherData} />
       <Loader />
     </section>
-  )
+  );
 };
 
 export default WeatherBox;
