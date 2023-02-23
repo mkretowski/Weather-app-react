f01ef10fe41d2494cbde966a5c22a25decffe86b
Mateusz Kretowski <matkretkurs@gmail.com>
2023-02-23 22:22:47 +0100

    Add statements for loader, WeatherSummary and ErrorBox display

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,36 +1,44 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import ErrorBox from '../ErrorBox/ErrorBox';
 import { useCallback } from 'react';
 import { useState } from 'react';
 const WeatherBox = (props) => {
-  const [weatherData, setWeatherData] = useState('');
-
-  const handleCityChange = useCallback(
-    (city) => {
-      //useCallback don't refresh chidren element PickCity when parent component WeatherBox is refreshed
-      fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d77f935b2221b70aa3dda57f36069a47&units=metric`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          const weather = {
+  const [weather, setWeather] = useState(false);
+  const [pending, setPending] = useState(false);
+  const [error, setError] = useState(false);
+  const handleCityChange = useCallback((city) => {
+    //useCallback don't refresh chidren element PickCity when parent component WeatherBox is refreshed
+    setPending(true);
+    setWeather(false);
+    setError(false);
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d77f935b2221b70aa3dda57f36069a47&units=metric`
+    ).then((res) => {
+      if (res.status === 200) {
+        return res.json().then((data) => {
+          const weatherData = {
             city: data.name,
             temp: data.main.temp,
             icon: data.weather[0].icon,
             description: data.weather[0].main,
           };
-          setWeatherData(weather);
+          setWeather(weatherData);
         });
-    },
-    [weatherData]
-  );
+      } else {
+        setError(true);
+      }
+      setPending(false);
+    });
+  }, []);
 
   return (
     <section>
       <PickCity action={handleCityChange} />
-      <WeatherSummary {...weatherData} />
-      <Loader />
+      {weather && !error && <WeatherSummary {...weather} />}
+      {!weather && pending && !error && <Loader />}
+      {error && <ErrorBox />}
     </section>
   );
 };
